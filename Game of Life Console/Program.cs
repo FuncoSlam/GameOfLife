04ce5d84c8806c1e5e279e36656af4ec4954a94f
FuncoSlam <gorvin.phey@gmail.com>
2021-11-05 04:00:39 -0600

    Add project files.

---
+++ Game of Life Console/Program.cs
@@ -0,0 +1,95 @@
+﻿
+using System.Diagnostics;
+using System.Linq;
+using GameOfLifeLibrary;
+
+public class Program
+{
+    public static void Main()
+    {
+        ParallelizationPerformanceTest();
+
+        static void OutputToConsole(GameOfLife gameOfLife, bool clearScreen = true)
+        {
+            bool[,] grid = gameOfLife.GetGrid();
+
+            string outputString = "";
+
+            outputString += "\n";
+            for (int x = 0; x < grid.GetLength(0); x++)
+            {
+                for (int y = 0; y < grid.GetLength(1); y++)
+                {
+                    if (grid[x, y]) outputString += "X";
+                    else outputString += "O";
+                }
+                outputString += "\n";
+            }
+            outputString += "\n\n";
+
+            if (clearScreen)
+                Console.Clear();
+
+            Console.Write(outputString);
+        }
+
+        static void ParallelizationPerformanceTest()
+        {
+            int cycle = 0;
+            int targetCycles = 1000;
+
+            Stopwatch stopwatch = new();
+
+            GameOfLife gameOfLife = new(20, 20);
+
+            gameOfLife.PopulateCell(2, 2);
+            gameOfLife.PopulateCell(3, 2);
+            gameOfLife.PopulateCell(1, 2);
+            gameOfLife.PopulateCell(3, 1);
+            gameOfLife.PopulateCell(2, 0);
+
+            GameOfLife gameOfLifeParallel = new(gameOfLife);
+
+            stopwatch.Start();
+            while (cycle < targetCycles)
+            {
+                gameOfLife.SimulateGameTick();
+                cycle++;
+            }
+            stopwatch.Stop();
+            bool[,] singleThreadState = gameOfLife.GetGrid();
+            long singleThreadTime = stopwatch.ElapsedMilliseconds;
+
+            cycle = 0;
+            stopwatch.Restart();
+            while (cycle < targetCycles)
+            {
+                gameOfLifeParallel.SimulateGameTickParallel();
+                cycle++;
+            }
+            stopwatch.Stop();
+            bool[,] multiThreadState = gameOfLifeParallel.GetGrid();
+            long multiThreadTime = stopwatch.ElapsedMilliseconds;
+
+            bool multiThreadEquality =
+                    singleThreadState.Rank == multiThreadState.Rank &&
+                    Enumerable.Range(0, singleThreadState.Rank).All(dimension => singleThreadState.GetLength(dimension) == multiThreadState.GetLength(dimension)) &&
+                    singleThreadState.Cast<bool>().SequenceEqual(multiThreadState.Cast<bool>());
+
+            Console.Write(
+                $"Running {targetCycles} cycles each:\n" +
+                $"\n" +
+                $"Single-threaded: {singleThreadTime}ms\n" +
+                $"Multi-threaded: {multiThreadTime}ms\n" +
+                $"\n" +
+                $"State Equality: {multiThreadEquality}\n"
+                );
+
+            OutputToConsole(gameOfLife, false);
+
+            OutputToConsole(gameOfLifeParallel, false);
+        }
+    }
+
+
+}
---
+++ Game of Life Library/GameOfLife.cs
@@ -0,0 +1,153 @@
+﻿using System.Threading.Tasks;
+
+namespace GameOfLifeLibrary;
+
+public class GameOfLife
+{
+    private bool edgeLooping;
+    private bool[,] currentGrid, nextGrid;
+    private static readonly (int, int)[] positionsToCheck = new (int, int)[8]
+    {
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1),           (0, 1),
+            (1, -1),  (1, 0),  (1, 1)
+    };
+
+    public GameOfLife(int x, int y, bool edgeLooping = true)
+    {
+        
+        currentGrid = new bool[x, y];
+        nextGrid = new bool[x, y];
+        this.edgeLooping = edgeLooping;
+    }
+
+    public GameOfLife(GameOfLife gameOfLife)
+    {
+        currentGrid = gameOfLife.currentGrid;
+        nextGrid = gameOfLife.nextGrid;
+        edgeLooping = gameOfLife.edgeLooping;
+    }
+
+    public void PopulateCell(int x, int y)
+    {
+        currentGrid[x, y] = true;
+    }
+
+    public void DepopulateCell(int x, int y)
+    {
+        currentGrid[x, y] = false;
+    }
+
+    public void ToggleCell(int x, int y)
+    {
+        currentGrid[x, y] = !currentGrid[x, y];
+    }
+
+    public void SimulateGameTick()
+    {
+        for (int x = 0; x < currentGrid.GetLength(0); x++)
+        {
+            for (int y = 0; y < currentGrid.GetLength(1); y++)
+            {
+                nextGrid[x, y] = ApplySurvivalRules(currentGrid[x, y], GetLivingNeighbors(x, y));
+            }
+        }
+
+        currentGrid = nextGrid.Clone() as bool[,];
+    }
+
+    public void SimulateGameTickParallel()
+    {
+        Parallel.For(0, currentGrid.GetLength(0), x =>
+        {
+            for (int y = 0; y < currentGrid.GetLength(1); y++)
+            {
+                nextGrid[x, y] = ApplySurvivalRules(currentGrid[x, y], GetLivingNeighbors(x, y));
+            }
+        });
+
+        currentGrid = nextGrid.Clone() as bool[,];
+    }
+
+    private int GetLivingNeighbors(int x, int y)
+    {
+        int activeNeighbors = 0;
+
+        foreach ((int,int) position in positionsToCheck)
+        {
+            (int, int) activeCell = (x + position.Item1, y + position.Item2);
+
+            if (IsPosInBounds(activeCell.Item1, activeCell.Item2))
+            {
+                if (currentGrid[activeCell.Item1, activeCell.Item2])
+                    activeNeighbors++;
+            }
+            else if (edgeLooping)
+            {
+                activeCell = ApplyEdgeLoopingToActiveCell(activeCell);
+                if (currentGrid[activeCell.Item1, activeCell.Item2])
+                    activeNeighbors++;
+            }
+        }
+
+        return activeNeighbors;
+
+    }
+
+    private (int, int) ApplyEdgeLoopingToActiveCell((int,int) activeCell)
+    {
+        if (activeCell.Item1 < 0)
+        {
+            activeCell.Item1 = currentGrid.GetLength(0) - 1;
+        }
+        else if (activeCell.Item1 >= currentGrid.GetLength(0))
+        {
+            activeCell.Item1 = 0;
+        }
+        if (activeCell.Item2 < 0)
+        {
+            activeCell.Item2 = currentGrid.GetLength(1) - 1;
+        }
+        else if (activeCell.Item2 >= currentGrid.GetLength(1))
+        {
+            activeCell.Item2 = 0;
+        }
+        return activeCell;
+    }
+
+
+
+    private static bool ApplySurvivalRules(bool currentState, int neighbors)
+    {
+        if (currentState)
+        {
+            if (neighbors == 2 || neighbors == 3) 
+                return true;
+            else 
+                return false;
+        }
+        else
+        {
+            if (neighbors == 3) 
+                return true;
+            else 
+                return false;
+        }
+    }
+
+    private bool IsPosInBounds(int x, int y)
+    {
+        if (x < 0 || y < 0) 
+            return false;
+
+        if (x >= currentGrid.GetLength(0) || y >= currentGrid.GetLength(1)) 
+            return false;
+
+        return true;
+    }
+
+    public bool[,] GetGrid()
+    {
+        return currentGrid;
+    }
+}
